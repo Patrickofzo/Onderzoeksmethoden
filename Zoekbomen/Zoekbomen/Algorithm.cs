75f4a87c810c44036986b9033a78a81e83e5009e
Patrickofzo <pat_pve@hotmail.com>
2015-02-24 16:25:25 +0100

    Start

---
+++ Zoekbomen/Zoekbomen/Algorithm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    abstract class Algorithm
+    {
+        abstract public int find(string a);
+        abstract public void delete(string a);
+        abstract public void add(string a);
+    }
+
+    class BTree : Algorithm
+    {
+        public override int find(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void delete(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void add(string a)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    class DancingTree : Algorithm
+    {
+        public override int find(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void delete(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void add(string a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class RedBlackTree : Algorithm
+    {
+        public override int find(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void delete(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void add(string a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class AVLTree : Algorithm
+    {
+        public override int find(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void delete(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void add(string a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class ScapeGoat : Algorithm
+    {
+        public override int find(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void delete(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void add(string a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class BinaryHeap : Algorithm
+    {
+        public override int find(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void delete(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void add(string a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+class Beap : Algorithm
+    {
+        public override int find(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void delete(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public override void add(string a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
