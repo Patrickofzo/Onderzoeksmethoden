4bd26dc276ce4ce4d190a686b299cdbaa57076cf
Stan Janssen <stanjanssen@outlook.com>
2015-02-26 17:30:37 +0100

    Avl, B and B Plus

--- onderzoek/onderzoek/Program.cs
+++ onderzoek/onderzoek/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.IO;
 
+//NuGet packages
+using CSharpTest.Net.Collections;
+using System.DataStructures;
+
 namespace onderzoek {
 	class MainClass {
 		public static void Main(string[] args) {
@@ -55,41 +59,50 @@ namespace onderzoek {
 	}
 
 	class BPlusTree<TData> : ISearchTree<TData> where TData : IComparable{
+		CSharpTest.Net.Collections.BPlusTree<TData, int> _tree;
 		public BPlusTree(TData[] data){
-			Console.WriteLine("Hello");
+			this._tree = new CSharpTest.Net.Collections.BPlusTree<TData, int>();
 		}
 
 		public void Add(TData element){
+			this._tree.Add(TData, 0);
 		}
 
 		public bool Find(TData element){
-			return false;
+			return this._tree.Contains(new KeyValuePair<TData, int>(element, 0));
 		}
 	}
 
 	class BTree<TData> : ISearchTree<TData> where TData : IComparable{
+		CSharpTest.Net.Collections.BTreeList<TData> _tree;
 		public BTree(TData[] data){
-			Console.WriteLine("Hello");
+			this._tree = new BTreeList<TData>();
+			foreach(TData ele in data){
+				this.Add(ele);
+			}
 		}
 
 		public void Add(TData element){
+			this._tree.Add(element);
 		}
 
 		public bool Find(TData element){
-			return false;
+			return this._tree.Contains(element);
 		}
 	}
 
 	class AvlTree<TData> : ISearchTree<TData> where TData : IComparable{
+		System.DataStructures.AvlTree<TData> _tree;
 		public AvlTree(TData[] data){
-			Console.WriteLine("Hello");
+			this._tree = new System.DataStructures.AvlTree<TData>(data);
 		}
 
 		public void Add(TData element){
+			this._tree.Add(element);
 		}
 
 		public bool Find(TData element){
-			return false;
+			return this._tree.Contains(element);
 		}
 	}
 
